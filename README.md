fa0f47da823fbfc6e2e5939c7cd556b98260cb61
Orlin Dimitrov <orlin.dimitrov@mail.bg>
2021-09-15 11:23:03 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# PhotoAppApiZuulAPIGatewayNew using spring-boot 2.1.3.RELEASE, in 2.5.4 it is depreciated
---
+++ src/test/java/com/or2d/photoapp/api/gateway/PhotoAppApiZuulAPIGatewayNew/PhotoAppApiZuulApiGatewayNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.or2d.photoapp.api.gateway.PhotoAppApiZuulAPIGatewayNew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiZuulApiGatewayNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
